44072c1b8ea461e1ec116f3da63b543157810c52
Anastasia Shironosova <a.shironosova@altsoft.spb.ru>
2021-04-05 15:19:54 +0300

    localStorage, адресная строка

--- src/App.vue
+++ src/App.vue
@@ -86,9 +86,24 @@
       </section>
       <template v-if="tickers.length > 0">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <div>
+          <button
+              class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+              v-if="page > 1"
+              @click="page = page - 1"
+          >Назад</button>
+          <button
+              class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+              v-if="hasNextPage"
+              @click="page = page + 1"
+
+          >Вперед</button>
+        </div>
+        <div>Фильтр:<input v-model="filter" /></div>
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             v-bind:key="t.name"
             @click="select(t)"
             :class="{
@@ -184,16 +199,49 @@ export default {
       graph: [],
       coinList: '',
       shortCoinList: '',
-      tickerAdded: false
+      tickerAdded: false,
+      page: 1,
+      filter: "",
+      hasNextPage: true
     };
   },
   async created() {
+    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries());
+    if (windowData.filter) {
+      this.filter = windowData.filter
+    }
+    if (windowData.page) {
+      this.page = windowData.page
+    }
     const coinList = await fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true');
     const data = await coinList.json();
     this.coinList = data.Data;
+    const tickersData = localStorage.getItem('cryptonomicon-list');
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach(ticker => {
+        this.subscribeToUpdates(ticker.name);
+      })
+
+    }
   },
 
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+            `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=5f3d7cba0329c4fd884c1b66ff9167d40be7fdedd2cebda4a95a2e7e79187106`
+        );
+        const data = await f.json();
+
+        this.tickers.find(t => t.name === tickerName).price =
+            data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 3000);
+      this.ticker = "";
+    },
     add(ticker) {
       const currentTicker = { name: ticker.toUpperCase(), price: "-" };
       let needAdd = true;
@@ -209,20 +257,11 @@ export default {
       if (needAdd) {
 
         this.tickers.push(currentTicker);
-        setInterval(async () => {
-          const f = await fetch(
-              `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=5f3d7cba0329c4fd884c1b66ff9167d40be7fdedd2cebda4a95a2e7e79187106`
-          );
-          const data = await f.json();
+        localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
 
-          this.tickers.find(t => t.name === currentTicker.name).price =
-              data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-          if (this.sel?.name === currentTicker.name) {
-            this.graph.push(data.USD);
-          }
-        }, 3000);
-        this.ticker = "";
+        this.subscribeToUpdates(currentTicker.name);
         this.shortCoinList = [];
+        this.filter = "";
       }
     },
     checkCoin (ticker) {
@@ -266,7 +305,27 @@ export default {
     select(ticker) {
       this.sel = ticker;
       this.graph = [];
+    },
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.filter.toUpperCase()));
+
+      this.hasNextPage = filteredTickers.length > end;
+
+
+      return filteredTickers.slice(start,end);
     }
+  },
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    },
+    page() {
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    },
   }
 };
 </script>
